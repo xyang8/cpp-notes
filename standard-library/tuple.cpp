e1929d4a03653a4f2571aff726750493a26b860b
xyang <121338860+xyang8@users.noreply.github.com>
2023-01-25 17:42:00 +0100

    Add more tuple examples

--- standard-library/tuple.cpp
+++ standard-library/tuple.cpp
@@ -2,11 +2,31 @@ import <string>;
 import <tuple>;
 import <iostream>;
 
+// std::tie can be used to define operator<=>
+class Foo {
+  public:
+    Foo(int i, std::string s, bool b) :
+        m_int{i},
+        m_str{std::move(s)},
+        m_bool{b} {
+    }
+
+    auto operator<=>(const Foo& rhs) const {
+        return std::tie(m_int, m_str, m_bool)
+           <=> std::tie(rhs.m_int, rhs.m_str, rhs.m_bool);
+    }
+
+  private:
+    int m_int;
+    std::string m_str;
+    bool m_bool;
+};
+
 auto main() -> int {
     using namespace std::string_literals;
-    auto t1 = std::make_tuple("eins"s, 1, 1.0, 1.0F);
+    auto t = std::make_tuple("eins"s, 1, 1.0, 1.0F);
 
-    [[maybe_unused]] auto t1_double = std::get<double>(t1);
+    [[maybe_unused]] auto t_double = std::get<double>(t);
 
     // Error if multiple elements of the requested type are contained:
     // auto t2 = std::make_tuple(1, 2);
@@ -21,4 +41,30 @@ auto main() -> int {
     int three{};
     int four{};
     std::tie(one, std::ignore, three, four) = std::make_tuple(1, 2, 3, 4);
+
+    std::cout << "Tuple size = "
+              << std::tuple_size<std::tuple<int, double, float>>::value
+              << '\n';
+    std::cout << "Tuple size = " << std::tuple_size<decltype(t)>::value
+              << '\n';
+
+    auto str{"a string"s};
+    [[maybe_unused]] auto tuple_with_ref = std::make_tuple(1, std::ref(str));
+    std::cout << std::get<1>(tuple_with_ref) << '\n';
+    str = "another string"s;
+    std::cout << std::get<1>(tuple_with_ref) << '\n';
+
+    [[maybe_unused]] auto sum = std::tuple_cat(t, tuple_with_ref);
+
+    auto f1 = Foo{1, "Hello"s, false};
+    auto f2 = Foo{1, "World"s, false};
+    std::cout << std::boolalpha << (f1 < f2) << '\n';
+
+    // Usefull for generic code using templates and template
+    // metaprogramming, t2 only needs to suport std::get<T>!!
+    auto t2 = std::make_tuple(1, "string", true);
+    auto f3 = std::make_from_tuple<Foo>(t2);
+
+    auto lambda = [](auto a, auto b) -> decltype(a + b) { return a + b; };
+    std::cout << std::apply(lambda, std::tuple{1, 2}) << '\n';
 }
